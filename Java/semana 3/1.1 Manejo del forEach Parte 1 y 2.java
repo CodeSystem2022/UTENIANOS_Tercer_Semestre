b917bff92e7cc038a2a2dc1c8a6055e8286180d6
Carolina Belén Delgado <98441984+programacioncarolina1980@users.noreply.github.com>
2023-06-04 19:32:05 -0300

    Update 1.1 Manejo del forEach Parte 1 y 2.java

--- Java/semana 3/1.1 Manejo del forEach Parte 1 y 2.java
+++ Java/semana 3/1.1 Manejo del forEach Parte 1 y 2.java
@@ -1,1 +1,10 @@
-
+/* For each semana 3
+ @author Semp=Carolina Delgado*/
+public class TestForEach {
+    public static void main (String[] args){
+         int edades[] ={5, 6, 8, 9 }; //Sintaxis resumida
+         for (int edad: edades) { //Sintaxis de ForEach
+              System.out.println ("edad = " + edad);
+         } 
+    }
+}
