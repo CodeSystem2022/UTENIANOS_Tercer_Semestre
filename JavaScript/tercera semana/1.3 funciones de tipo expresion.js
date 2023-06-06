bccb63f64e1064bfa6be389f5af3a5426f4fd126
Diego Agustin Putrino Chirivino <agussputrii@gmail.com>
2023-06-06 13:34:40 -0300

    Update 1.3 funciones de tipo expresion.js

--- JavaScript/tercera semana/1.3 funciones de tipo expresion.js
+++ JavaScript/tercera semana/1.3 funciones de tipo expresion.js
@@ -1,1 +1,4 @@
-
+// Declaramos una función de tipo expresión o anónima
+let x = function(a, b){return a + b}; // necesita cierre con punto y coma
+resultado = x(5, 6); // al llamarla se pone la variable y paréntesis
+console.log(resultado);
