158f1a035e23cd5b5bd87a7f60fa34ab8f892dc3
Diego Agustin Putrino Chirivino <agussputrii@gmail.com>
2023-06-06 14:17:20 -0300

    Update 1.6 Funciones f lecha.js

--- JavaScript/tercera semana/1.6 Funciones f lecha.js
+++ JavaScript/tercera semana/1.6 Funciones f lecha.js
@@ -2,12 +2,3 @@
 const sumarFuncionFlecha = (a, b) => a + b;
 resultado = sumarFuncionFlecha(3,7); //Asignamos el valor a una variable
 console.log(resultado);
-
-//Función tipo expresión
-let sumar = function(a = 4, b = 8){
-    console.log(arguments[0]); //Muestra el parámetro de: a
-    console.log(arguments[1]); //muestra el parámetro de: b
-    return a + b + arguments[2];
-}
-resultado = sumar(3, 2, 9);
-console.log(resultado);
