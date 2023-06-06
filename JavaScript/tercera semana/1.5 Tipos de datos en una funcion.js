1b1b9522cfb210bfb0a47f4f0c670ad10acd66f3
Diego Agustin Putrino Chirivino <agussputrii@gmail.com>
2023-06-06 14:16:44 -0300

    Update 1.5 Tipos de datos en una funcion.js

--- JavaScript/tercera semana/1.5 Tipos de datos en una funcion.js
+++ JavaScript/tercera semana/1.5 Tipos de datos en una funcion.js
@@ -1,1 +1,10 @@
+console.log(typeof miFuncion); function
+function miFuncionDos(a, b){
+    console.log(arguments.length)
+}
 
+miFuncionDos(5, 7, 3, 6);
+
+//toString
+var miFuncionTexto = miFuncionDos.toString();
+console.log(miFuncionTexto);
