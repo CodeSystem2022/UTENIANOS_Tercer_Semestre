1ae13bc77613e3eeda37e173d8b5297ad82a40ff
Carolina Belén Delgado <98441984+programacioncarolina1980@users.noreply.github.com>
2023-06-08 19:12:25 -0300

    Update Clase hija Ratón

--- Java/Semana 1/Clase hija Ratón y la clase Teclado.java
+++ Java/Semana 1/Clase hija Ratón y la clase Teclado.java
@@ -1,1 +1,22 @@
 
+package ar.com.systems2023.mundopc;
+
+/**
+ * @author Semp = Caro Delgado
+ */
+public class Raton extends DispositivoEntrada{
+   private final int idRaton;
+   private static int contadorRatones;
+   
+   public Raton( String tipoEntrada, String marca){
+        super(tipoEntrada, marca);
+        this.idRaton = ++Raton.contadorRatones;
+   }
+
+@Override
+public String toString() {
+return "Raton{" + "idRaton=" + idRaton +", "+super.toString()+'}';
+}
+
+}
+
