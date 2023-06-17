ac9e1cf7a2d6ac1f94ff682b11f7f0597af76b66
Miriam <111610111+MiriamTorres30@users.noreply.github.com>
2023-06-17 18:10:31 -0300

    Ejercicio Figura Geométrica

---
+++ Java/Leccion05/FiguraGeometrica.java
@@ -0,0 +1,28 @@
+
+package domain;
+
+
+public abstract class FiguraGeometrica {
+    protected String tipoFigura;
+    
+    protected FiguraGeometrica(String tipoFigura){
+        this.tipoFigura = tipoFigura;
+    }
+    // Método abstracto
+    public abstract void dibujar();
+    
+    // Agregamos el get y el set
+
+    public String getTipoFigura() {
+        return tipoFigura;
+    }
+
+    public void setTipoFigura(String tipoFigura) {
+        this.tipoFigura = tipoFigura;
+    }
+
+    @Override
+    public String toString() {
+        return "FiguraGeometrica{" + "tipoFigura=" + tipoFigura + '}';
+    }  
+}
