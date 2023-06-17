92e6e2eb809ff4364a8e08eadebc1b025dc122d9
Miriam <111610111+MiriamTorres30@users.noreply.github.com>
2023-06-17 18:26:26 -0300

    TestExcepciones.java

--- Java/semana 8/TestExcepciones.java
+++ Java/semana 8/TestExcepciones.java
@@ -1,1 +1,24 @@
+package test;
 
+import static aritmetica.Aritmetica.division;
+import excepciones.OperacionExcepcion;
+
+public class TestExcepciones {
+    public static void main(String[] args) {
+        int resultado = 0;
+        try{
+           resultado = division(10, 0);
+        }catch(OperacionExcepcion e){
+            System.out.println("Ocurrió un error de tipo OperacionExcepcion");
+            System.out.println(e.getMessage());
+        }catch(Exception e){
+            System.out.println("Ocurrió un Error");
+            e.printStackTrace(System.out);  // Se conoce como la pila de excepciones
+            System.out.println(e.getMessage());
+        }
+        finally{
+            System.out.println("Se revisó la división entre cero");
+        }
+        System.out.println("La variable de resultado tiene como valor: "+resultado);
+    }
+}
