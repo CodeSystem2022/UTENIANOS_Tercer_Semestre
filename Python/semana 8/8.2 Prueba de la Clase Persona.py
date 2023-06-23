e2e9c2ce3eaf8683f70e83d6ad985aba128f2e57
Diego Agustin Putrino Chirivino <85753366+agussputrii@users.noreply.github.com>
2023-06-22 22:19:32 -0300

    Update 8.2 Prueba de la Clase Persona.py

--- Python/semana 8/8.2 Prueba de la Clase Persona.py
+++ Python/semana 8/8.2 Prueba de la Clase Persona.py
@@ -1,1 +1,6 @@
-
+if __name__ == '__main__':
+    log.debug('Mensaje a nivel Debug')
+    log.info('Mensaje a nivel Info')
+    log.warning('Mensaje a nivel Warning')
+    log.error('Mensaje a nivel Error')
+    log.critical('Mensaje a nivel Critical')
