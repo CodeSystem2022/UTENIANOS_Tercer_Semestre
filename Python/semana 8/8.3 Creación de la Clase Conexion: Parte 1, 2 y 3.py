fd5793a272db9f7dc7963d0132de7321000de8c5
Diego Agustin Putrino Chirivino <85753366+agussputrii@users.noreply.github.com>
2023-06-22 22:20:56 -0300

    Update 8.3 Creación de la Clase Conexion: Parte 1, 2 y 3.py

--- Python/semana 8/8.3 Creación de la Clase Conexion: Parte 1, 2 y 3.py
+++ Python/semana 8/8.3 Creación de la Clase Conexion: Parte 1, 2 y 3.py
@@ -1,1 +1,48 @@
+class Conexion:
+    _DATABASE = 'test_bd'
+    _USERNAME = 'postgres'
+    _PASSWORD = 'admin'
+    _DB_PORT = '5432'
+    _HOST = '127.0.0.1'
+    _MIN_CON = 1
+    _MAX_CON = 5
+    _pool = None
+    #Método para la conexion
+    @classmethod
+    def obtenerConexion(cls):
+        if cls._conexion is None:
+            try:
+                cls._conexion = bd.connect(host=cls._HOST,
+                                           user=cls._USERNAME,
+                                           password=cls._PASSWORD,
+                                           port=cls._DB_PORT,
+                                           database=cls._DATABASE)
+                log.debug(f'Conexión Exitosa: {cls._conexion}')
+                return cls._conexion   # mostramos el tipo de conexion
+            except Exception as e:
+                log.error(f'Ocurrió un error: {e}')
+                sys.exit()
+        else:
+            return cls._conexion
 
+    #Método para el cursor
+
+    @classmethod
+    def obtenerCursor(cls):
+        if cls._cursor is None:
+            try:
+                cls._cursor = cls.obtenerConexion().cursor()
+                log.debug(f'Se abrió correctamente el cursor: {cls._cursor}')
+                return cls._cursor
+            except Exception as e:
+                log.error(f'Ocurrió un error: {e}')
+                sys.exit()
+        else:
+            return cls._cursor
+
+
+# Prueba de errores de la clase Conection:
+
+if __name__ == '__main__':
+    Conexion.obtenerConexion()
+    Conexion.obtenerCursor()
