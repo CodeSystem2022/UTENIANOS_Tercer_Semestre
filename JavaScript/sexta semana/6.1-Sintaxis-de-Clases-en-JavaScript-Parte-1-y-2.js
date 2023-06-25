272b4e862f6cccae40576f622a06465ca8b644be
Diego Agustin Putrino Chirivino <85753366+agussputrii@users.noreply.github.com>
2023-06-25 20:44:47 -0300

    Update 6.1-Sintaxis-de-Clases-en-JavaScript-Parte-1-y-2.js

--- JavaScript/sexta semana/6.1-Sintaxis-de-Clases-en-JavaScript-Parte-1-y-2.js
+++ JavaScript/sexta semana/6.1-Sintaxis-de-Clases-en-JavaScript-Parte-1-y-2.js
@@ -1,1 +1,60 @@
+// Clase Persona
+class Persona {
+  constructor(nombre, apellido) {
+    this._nombre = nombre;
+    this._apellido = apellido;
+  }
 
+  get nombre() {
+    return this._nombre;
+  }
+
+  set nombre(nombre) {
+    this._nombre = nombre;
+  }
+
+  get apellido() {
+    return this._apellido;
+  }
+
+  set apellido(apellido) {
+    this._apellido = apellido;
+  }
+}
+
+// Clase Empleado que hereda de Persona
+class Empleado extends Persona {
+  constructor(nombre, apellido, departamento) {
+    super(nombre, apellido);
+    this._departamento = departamento;
+  }
+
+  get departamento() {
+    return this._departamento;
+  }
+
+  set departamento(departamento) {
+    this._departamento = departamento;
+  }
+}
+
+// Creación de objetos y pruebas
+let persona1 = new Persona('Martín', 'Perez');
+console.log(persona1.nombre);
+persona1.nombre = 'Juan Carlos';
+console.log(persona1.nombre);
+console.log(persona1.apellido);
+persona1.apellido = 'Alvarez';
+console.log(persona1.apellido);
+
+let persona2 = new Persona('Carlos', 'Lara');
+console.log(persona2.nombre);
+persona2.nombre = 'Maria Laura';
+console.log(persona2.nombre);
+console.log(persona2.apellido);
+persona2.apellido = 'Luna';
+console.log(persona2.apellido);
+
+let empleado1 = new Empleado('María', 'Gimenez', 'Sistema');
+console.log(empleado1);
+console.log(empleado1.nombre);
