cc2476c51746985d15a011a1ca4b1eb9fd32fefb
Diego Agustin Putrino Chirivino <85753366+agussputrii@users.noreply.github.com>
2023-07-05 17:30:25 -0300

    Update 6.1-Correccion-de-mensajes-de-error-en-Postgres-Parte-1-y-2.py

--- Python/semana 6/6.1-Correccion-de-mensajes-de-error-en-Postgres-Parte-1-y-2.py
+++ Python/semana 6/6.1-Correccion-de-mensajes-de-error-en-Postgres-Parte-1-y-2.py
@@ -1,1 +1,57 @@
+import psycopg2 as bd
+
+# Escenario 1: Utilizando bloque with para la transacción y rollback automático en caso de excepción
+conexion = bd.connect(user='postgres', password='admin', host='127.0.0.1', port='5432', database='test_bd')
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            sentencia = 'INSERT INTO persona(nombre, apellido, email) VALUES (%s, %s, %s)'
+            valores = ('Alex', 'Rojas', 'arojas@mail.com')
+            cursor.execute(sentencia, valores)
+
+            sentencia = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
+            valores = ('Juan Carlos', 'Roldan', 'jroldan@gmail.com', 1)
+            cursor.execute(sentencia, valores)
+            
+except Exception as e:
+    print(f'Ocurrió un error, se hizo un rollback: {e}')
+finally:
+    conexion.close()
+
+# Escenario 2: Realizando commit manualmente y rollback en caso de excepción
+conexion = bd.connect(user='postgres', password='admin', host='127.0.0.1', port='5432', database='test_bd')
+try:
+    conexion.autocommit = False
+    cursor = conexion.cursor()
+    sentencia = 'INSERT INTO persona(nombre, apellido, email) VALUES (%s, %s, %s)'
+    valores = ('María', 'Esparza', 'mesparza@mail.com')
+    cursor.execute(sentencia, valores)
+    conexion.commit()
+    print('Termina la transacción')
+except Exception as e:
+    conexion.rollback()
+    print(f'Ocurrió un error, se hizo un rollback: {e}')
+finally:
+    conexion.close()
+
+# Escenario 3: Utilizando bloque with para la transacción y realizando commit manualmente
+conexion = bd.connect(user='postgres', password='admin', host='127.0.0.1', port='5432', database='test_bd')
+try:
+    conexion.autocommit = False
+    cursor = conexion.cursor()
+    sentencia = 'INSERT INTO persona(nombre, apellido, email) VALUES (%s, %s, %s)'
+    valores = ('Jorge', 'Prol', 'jprol@mail.com')
+    cursor.execute(sentencia, valores)
+
+    sentencia = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
+    valores = ('Juan Carlos', 'Perez', 'jcperez@gmail.com', 1)
+    cursor.execute(sentencia, valores)
+
+    conexion.commit()
+    print('Termina la transacción')
+except Exception as e:
+    conexion.rollback()
+    print(f'Ocurrió un error, se hizo un rollback: {e}')
+finally:
+    conexion.close()
 
