0138912c9233be9c320fe36c2e927c61517509a5
Diego Agustin Putrino Chirivino <85753366+agussputrii@users.noreply.github.com>
2023-07-05 17:22:52 -0300

    Create insertar_registro_psycopg2.py

---
+++ Python/semana 5/insertar_registro_psycopg2.py
@@ -0,0 +1,16 @@
+import psycopg2
+
+conexion = psycopg2.connect(user='postgres', password='admin', host='127.0.0.1', port='5432', database='test_bd')
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            sentencia = 'INSERT INTO persona (nombre, apellido, email)VALUES (%s, %s, %s)'
+            valores = ('Carlos', 'Lara', 'clara@mail.com') #Es una tupla
+            cursor.execute(sentencia, valores) # ejecutamos la sentencia
+            # conexion.commit() guardar los cambios en la base de datos
+            registros_insertados = cursor.rowcount
+            print(f'Los registros insertados son: {registros_insertados}')
+except Exception as e:
+    print(f'Ocurrió un error: {e}')
+finally:
+    conexion.close()
