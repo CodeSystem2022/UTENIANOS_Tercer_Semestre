e45d9b86e68fa3f0fdf11b0e19920b8dfbbcdc1f
Carolina Belén Delgado <98441984+programacioncarolina1980@users.noreply.github.com>
2023-07-05 10:38:18 -0300

    Delete Palabra reservada Continue.js
    Cambio de carpeta

--- Java/Semana 1/Palabra reservada Continue.js
+++
@@ -1,8 +0,0 @@
-inicio:
-for (let contado = 0; contado <= 10; contado++) {
-    if (contado % 2 !== 0) {
-        continue inicio;
-    }
-    console.log(contado);
-}
-console.log("Termina el ciclo al encontrar un número par");
