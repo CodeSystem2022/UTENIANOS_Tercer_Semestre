411a382708447743ad8fa9d8708368bdbd7bc611
David <111609786+DavidTorres92@users.noreply.github.com>
2023-07-05 21:10:20 -0300

    Ejercicio Clase computadora Semana 1

--- Java/Semana 1/Clase computadora.java
+++ Java/Semana 1/Clase computadora.java
@@ -1,1 +1,69 @@
+package ar.com.system2023.mundopc;
 
+public class Computadora {
+    private final int idComputadora;
+    private String nombre;
+    private Monitor monitor;
+    private Teclado teclado;
+    private Raton raton;
+    private static int contadorComputadoras;
+
+    //contador vacio
+    public Computadora() {
+        this.idComputadora = ++Computadora.contadorComputadoras;
+    }
+    
+    //Const 2
+
+    public Computadora(String nombre, Monitor monitor, Teclado teclado, Raton raton) {
+        this();
+        this.nombre = nombre;
+        this.monitor = monitor;
+        this.teclado = teclado;
+        this.raton = raton;
+    }
+
+    //Get id Computadora
+    public int getIdComputadora() {
+        return this.idComputadora;
+    }
+    
+    //Getters & Setters 
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Monitor getMonitor() {
+        return monitor;
+    }
+
+    public void setMonitor(Monitor monitor) {
+        this.monitor = monitor;
+    }
+
+    public Teclado getTeclado() {
+        return teclado;
+    }
+
+    public void setTeclado(Teclado teclado) {
+        this.teclado = teclado;
+    }
+
+    public Raton getRaton() {
+        return raton;
+    }
+
+    public void setRaton(Raton raton) {
+        this.raton = raton;
+    }
+
+    @Override
+    public String toString() {
+        return "Computadora{" + "idComputadora=" + idComputadora + ", nombre=" + nombre + ", monitor=" + monitor + ", teclado=" + teclado + ", raton=" + raton + '}';
+    }
+        
+}
