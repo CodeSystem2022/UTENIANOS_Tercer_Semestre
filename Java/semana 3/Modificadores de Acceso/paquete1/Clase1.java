3395302451df3e0c4dc3e31c5ea2cc7f10537de6
David <111609786+DavidTorres92@users.noreply.github.com>
2023-07-05 23:55:00 -0300

    Clase1.java

---
+++ Java/semana 3/Modificadores de Acceso/paquete1/Clase1.java
@@ -0,0 +1,31 @@
+package paquete1;
+
+public class Clase1 {
+    
+    private String atributoPrivado = "Este es un atributo privado";
+    String atributoPaquete = "Este es un atributo de paquete";
+    protected String atributoProtegido = "Este es un atributo protegido";
+    public String atributoPublico = "Este es un atributo público";
+    
+    // Constructor de la clase
+    public Clase1() {
+        System.out.println("Se ha creado una instancia de la clase Clase1");
+    }
+    
+    // Métodos de la clase
+    private void metodoPrivado() {
+        System.out.println("Este es un método privado");
+    }
+    
+    void metodoPaquete() {
+        System.out.println("Este es un método de paquete");
+    }
+    
+    protected void metodoProtegido() {
+        System.out.println("Este es un método protegido");
+    }
+    
+    public void metodoPublico() {
+        System.out.println("Este es un método público");
+    }
+}
