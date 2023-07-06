a2f6a57c2d12f77d59463bd80b560a7292b92753
David <111609786+DavidTorres92@users.noreply.github.com>
2023-07-06 00:00:57 -0300

    Add files via upload

---
+++ Java/semana 3/Modificadores de Acceso /Paquete1/Clase2.java
@@ -0,0 +1,16 @@
+package paquete1;
+
+public class Clase2 extends Clase1 {
+    String atributoDefault = "Valor del atributo default";
+    
+    
+    public Clase2(){
+        super();
+        this.atributoDefault = "Modificacion del atributo default";
+        System.out.println("atributoDefault = " + this.atributoDefault);
+        this.metodoDefault();   
+    }
+    void metodoDefault(){
+        System.out.println("Metodo Default");
+    }
+}
