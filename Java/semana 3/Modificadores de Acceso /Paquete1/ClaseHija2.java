d24f91ce8076bd5e9f2aca5383b4fb13ce1cf79c
David <111609786+DavidTorres92@users.noreply.github.com>
2023-07-06 00:01:35 -0300

    Add files via upload

---
+++ Java/semana 3/Modificadores de Acceso /Paquete1/ClaseHija2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete1;
+
+/**
+ *
+ * @author agus
+ */
+public class ClaseHija2 extends Clase2 {
+    
+    public ClaseHija2(){
+        super();
+        this.atributoDefault = "Mod atr def";
+        System.out.println("AD = "+this.atributoDefault);
+        this.metodoDefault();
+    }
+
+}
